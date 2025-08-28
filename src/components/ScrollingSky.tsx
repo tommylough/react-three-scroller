5880b59562d55681287f81576fca5d67f86ae68c
Tommy <thomas.m.lough@gmail.com>
2025-08-28 09:01:41 -0700

    Added plane, sky png and scree resize event
    Adds plane geometry to hold sky
    Adds sky png image for plane material
    Adds screen resize event listener

--- src/components/ScrollingSky.tsx
+++ src/components/ScrollingSky.tsx
@@ -3,14 +3,16 @@ import * as THREE from "three";
 
 interface SkyProps {
   scrollProgress: number;
+  width: number;
 }
 
-export const ScrollingSky = ({ scrollProgress }: SkyProps) => {
+export const ScrollingSky = ({ scrollProgress, width }: SkyProps) => {
   const opacity = 1 - scrollProgress;
   const texture = useLoader(THREE.TextureLoader, "/images/sky.png");
+  console.log(`Width: ${width / 45}`);
   return (
     <mesh position={[0, 8, -5]}>
-      <planeGeometry args={[30, 20]} />
+      <planeGeometry args={[width / 45, 20]} />
       <meshBasicMaterial transparent={true} map={texture} opacity={opacity} />
     </mesh>
   );
